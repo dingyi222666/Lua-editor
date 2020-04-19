7c9f83426814a3040257925b0cf27a4cc1d48c59
dingyi <2960586094@qq.com>
2020-04-19 19:19:47 +0800

    更新

---
+++ java/com/lua/editor/MainActivity.java
@@ -0,0 +1,10 @@
+package com.lua.editor;
+import android.app.*;
+import android.os.*;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
